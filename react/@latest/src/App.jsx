6aa87a218da0a483b13584fa874cb938cb91956b
chechu776 <shameerchechu@gmail.com>
2025-09-17 10:14:12 +0530

    useReducer

--- react/@latest/src/App.jsx
+++ react/@latest/src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios"
 import { createContext } from "react";
 import { useContext } from "react";
+import { useReducer } from "react";
 
 const app = () => {
   // const [users,setUsers]=useState([])
@@ -48,25 +49,46 @@ const app = () => {
 //       </button>
 //   )
 
-return <>
-  <Con.Provider value="shanu"><Child /></Con.Provider>
-  <Con.Provider value="sha"><Child2 /></Con.Provider>
-</>
+// return <>
+//   <Con.Provider value="shanu"><Child /></Con.Provider>
+//   <Con.Provider value="sha"><Child2 /></Con.Provider>
+// </>
+
+const reducer =(state,action)=>{
+  if(action == "inc")
+  {
+    return state+1
+  }
+  else
+  {
+    return state-1
+  }
+}
+
+const [state,dispatch] = useReducer(reducer,0)
+
+return(
+  <>
+    <h1> count :{state}</h1>
+    <button onClick={()=>dispatch("inc")}> increment</button>
+    <button onClick={()=>dispatch("dec")}> decrement</button>
+  </>
+)
 
 }
 
 
 export default app;
 
-const Con = createContext()
+// const Con = createContext()
 
-const Child = ()=>{
-  const name = useContext(Con)
-  return <h1>helllo : {name}</h1>
-}
+// const Child = ()=>{
+//   const name = useContext(Con)
+//   return <h1>helllo : {name}</h1>
+// }
 
-const Child2 = ()=>{
-  const name = useContext(Con);
-  return <h1>hello : {name}</h1>
-}
+// const Child2 = ()=>{
+//   const name = useContext(Con);
+//   return <h1>hello : {name}</h1>
+// }
 
