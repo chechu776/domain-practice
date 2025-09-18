92ae4aface23c1f21b9a1ee896aecd001b730bee
chechu776 <shameerchechu@gmail.com>
2025-09-18 11:33:04 +0530

    loops revised

--- map.js
+++ map.js
@@ -1,7 +1,48 @@
-const arr = ["shanu","shamsudheen","nadhil","riyas"]
+// const arr = ["shanu","shamsudheen","nadhil","riyas"]
 
-const arr2 = arr.filter((e)=>{
-    return e.startsWith("s")
-})
+// const arr2 = arr.filter((e)=>{
+//     return e.startsWith("s")
+// })
 
-console.log("",arr2);
+// console.log("",arr2);
+
+const a = [ 1,2,4,5,6,8]
+
+const ne = a.reduce((e,s)=>{
+    return e+s
+},0)
+
+console.log(ne);
+
+// a.forEach((e,i)=>{
+//     console.log(e,i);
+// })
+
+// for(let e of a )
+// {
+//     console.log(e);
+    
+// }
+
+const stud = {name:"chechu", age:20}
+
+stud.name = "sameer"
+
+for ( let a in stud)
+{
+    console.log(a ,":", stud[a]);
+    
+}
+
+// stud.address = " kochi "
+
+// for ( let a in stud)
+// {
+//     console.log(a ,":", stud[a]);
+// }
+
+const copy = {...stud}
+
+console.log("copy" , copy);
+
+
