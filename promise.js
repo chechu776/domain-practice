52f0c260364542f1918b393bbafd64f0f15b89b5
chechu776 <shameerchechu@gmail.com>
2025-09-18 16:58:46 +0530

    promise

--- promise.js
+++ promise.js
@@ -19,17 +19,46 @@
 //     catch(err)
 //     {
 //         console.log(err);
-        
+
 //     }
 // }
 
 // display()
 
-const p1 =  Promise.resolve(10)
-const p2 =  Promise.resolve(20)
-const p3 =  Promise.resolve(40)
+// const p1 = Promise.resolve(10)
+// const p2 = Promise.resolve(20)
+// const p3 = Promise.resolve(40)
 
-Promise.all([p1,p2,p3]).then((res)=>{console.log(res)})
-.catch((error)=>{
-    console.log("error",error);
-})
+// Promise.all([p1, p2, p3]).then((res) => { console.log(res) })
+//     .catch((error) => {
+//         console.log("error", error);
+//     })
+
+const app = (a) => {
+    return new Promise((res, rej) => {
+        if (a == 0)
+            res("promise resolved")
+        else {
+            rej("promise rejected");
+        }
+    })
+}
+
+const promisedisplay=async()=>{
+    try {
+        const result = await app(0);
+        console.log(result);
+        
+    }
+    catch(err)
+    {
+        console.log(err);
+        
+    }
+}
+
+const newpro =(a)=>{
+    return new Promise
+}
+
+promisedisplay()
