d149980e531698396c5e3efbf19d4c4eddc798a1
chechu776 <shameerchechu@gmail.com>
2025-09-19 10:38:50 +0530

    string reversed and palindrome chekd

--- reverse.js
+++ reverse.js
@@ -1,9 +1,40 @@
-const a = [1,2,3,4,5,6]
+// const a = [1,2,3,4,5,6]
 
-for(let i = 0 ; i <= (a.length-1)/2 ; i++)
+// for(let i = 0 ; i <= (a.length-1)/2 ; i++)
+// {
+//     let temp = a[i];
+//     a[i]=a[a.length-1-i];
+//     a[a.length-1-i]=temp
+// }
+// console.log("reversed array : ",a);
+
+const s = "heh"
+
+const s2 = s.split("")
+
+console.log(s2);
+
+for(let i = 0 ; i <= (s2.length-1)/2 ; i++)
 {
-    let temp = a[i];
-    a[i]=a[a.length-1-i];
-    a[a.length-1-i]=temp
+    let temp =s2[i];
+    s2[i]= s2[s2.length-1-i];
+    s2[s2.length-1-i]=temp
 }
-console.log("reversed array : ",a);
+
+console.log("new reversed array is ",s2);
+
+const s3 = s2.join("")
+
+console.log(s3);
+
+
+if(s==s3)
+{
+    console.log("palindrome");
+    
+}
+else
+{
+    console.log("not palindrome");
+    
+}
